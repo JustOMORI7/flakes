e90236800f1f7a46c8bc26d66aff9e8ef2dd86f6
JustOMORI7 <162158604+JustOMORI7@users.noreply.github.com>
2025-09-24 21:27:08 +0300

    Update main.py

--- ymdown/main.py
+++ ymdown/main.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 import gi
-gi.require_version("Gtk", "4.0")
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 import subprocess
 import os
 
 download_dir = os.path.expanduser("~/Music")
 
-class Downloader(Gtk.ApplicationWindow):
-    def __init__(self, app):
-        super().__init__(application=app, title="You Music Downloader")
+class Downloader(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="You Music Downloader")
         self.set_default_size(500, 200)
 
         self.url_entry = Gtk.Entry()
@@ -21,65 +21,59 @@ class Downloader(Gtk.ApplicationWindow):
         vbox.set_margin_bottom(10)
         vbox.set_margin_start(10)
         vbox.set_margin_end(10)
-        self.set_child(vbox)
+        self.add(vbox)
 
         # URL label sola yaslı
         label_url = Gtk.Label(label="YouTube Link:")
         label_url.set_xalign(0)
-        vbox.append(label_url)
-        vbox.append(self.url_entry)
+        vbox.pack_start(label_url, False, False, 0)
+        vbox.pack_start(self.url_entry, False, False, 0)
 
         folder_button = Gtk.Button(label="Select folder")
         folder_button.connect("clicked", self.choose_directory)
-        vbox.append(folder_button)
+        vbox.pack_start(folder_button, False, False, 0)
 
         # Folder label sola yaslı
         self.folder_label = Gtk.Label(label=f"Selected folder: {download_dir}")
         self.folder_label.set_xalign(0)
-        vbox.append(self.folder_label)
+        vbox.pack_start(self.folder_label, False, False, 0)
 
         # Buttons
         hbox = Gtk.Box(spacing=10)
-        label_dl = Gtk.Label(label="Download as:")
-        label_dl.set_xalign(0)
-        hbox.append(label_dl)
+        hbox.pack_start(Gtk.Label(label="Download as:"), False, False, 0)
 
         m4a_button = Gtk.Button(label="M4A")
         m4a_button.connect("clicked", lambda w: self.download_audio("m4a"))
-        hbox.append(m4a_button)
+        hbox.pack_start(m4a_button, False, False, 0)
 
         mp3_button = Gtk.Button(label="MP3")
         mp3_button.connect("clicked", lambda w: self.download_audio("mp3"))
-        hbox.append(mp3_button)
+        hbox.pack_start(mp3_button, False, False, 0)
 
-        vbox.append(hbox)
+        vbox.pack_start(hbox, False, False, 0)
 
     def choose_directory(self, widget):
-        dialog = Gtk.FileChooserNative(
+        dialog = Gtk.FileChooserDialog(
             title="Select folder",
-            transient_for=self,
+            parent=self,
             action=Gtk.FileChooserAction.SELECT_FOLDER,
-            accept_label="Select",
-            cancel_label="Cancel"
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_OK, Gtk.ResponseType.OK
         )
 
-        def on_response(dlg, response):
-            if response == Gtk.ResponseType.ACCEPT:
-                global download_dir
-                file = dlg.get_file()
-                if file:
-                    download_dir = file.get_path()
-                    self.folder_label.set_text(f"Selected folder: {download_dir}")
-            dlg.destroy()
+        if dialog.run() == Gtk.ResponseType.OK:
+            global download_dir
+            download_dir = dialog.get_filename()
+            self.folder_label.set_text(f"Selected folder: {download_dir}")
 
-        dialog.connect("response", on_response)
-        dialog.show()
-
+        dialog.destroy()
 
     def download_audio(self, format_type):
         url = self.url_entry.get_text().strip()
         if not url:
-            self.show_message("Error", "Please enter a YouTube link.", Gtk.MessageType.ERROR)
+            self.show_message("Error", "Please enter a YouTube link.")
             return
 
         outtmpl = os.path.join(download_dir, "%(title)s.%(ext)s")
@@ -98,40 +92,28 @@ class Downloader(Gtk.ApplicationWindow):
 
         try:
             subprocess.run(command, check=True)
-            self.show_message("Successful", f"Download completed!\n{download_dir}", Gtk.MessageType.INFO)
+            self.show_message("Successful", f"Download completed!\n{download_dir}")
         except subprocess.CalledProcessError:
-            self.show_message("Error", "There was a problem during the download.", Gtk.MessageType.ERROR)
+            self.show_message("Error", "There was a problem during the download.")
 
-    def show_message(self, title, message, msg_type):
+    def show_message(self, title, message):
         dialog = Gtk.MessageDialog(
             transient_for=self,
-            modal=True,
-            message_type=msg_type,
+            flags=0,
+            message_type=Gtk.MessageType.INFO if title == "Successful" else Gtk.MessageType.ERROR,
             buttons=Gtk.ButtonsType.OK,
             text=title,
         )
-
-        if message:
-            dialog.set_markup(f"<b>{title}</b>\n\n{message}")
-
-        dialog.connect("response", lambda d, r: d.destroy())
-        dialog.show()
-
-
-
-class DownloaderApp(Gtk.Application):
-    def __init__(self):
-        super().__init__(application_id="com.example.YouMusicDownloader")
-
-    def do_activate(self):
-        win = Downloader(self)
-        win.present()
+        dialog.format_secondary_text(message)
+        dialog.run()
+        dialog.destroy()
 
 
 def main():
-    app = DownloaderApp()
-    app.run()
-
+    win = Downloader()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
 
 if __name__ == "__main__":
     main()
